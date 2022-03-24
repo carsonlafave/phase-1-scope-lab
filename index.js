02e582d252bba898fcb6a287cb103b2d0db383ec
carsonlafave <clafaveaz@gmail.com>
2022-03-24 13:36:56 -0700

    commit tree 8dd032f287e1251a0be1f1824c684b806a8613a6

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+
+   
+
+
+var bestCustomer;
+function setBestCustomer(){
+    return bestCustomer = 'not bob';
+
+}
+
+function upperCaseCustomerName(){
+    var upperCaseCustomerName = customerName.toUpperCase()
+    return upperCaseCustomerName
+}
+
+
+function  overwriteBestCustomer(){
+    const bestCustomer = 'frank';
+}
+
+const leastFavortieCustomer = 'michael';
+
+function changeLeastFavoriteCustomer(){
+    leastFavortieCustomer = 'Matthew';
+    return leastFavortieCustomer;
+}
+
+
+
+        
+
+ 
